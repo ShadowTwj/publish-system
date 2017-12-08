020c41668bcecb307d3412c8bf694fba81a32256
韩明泽 <hanmz@fxiaoke.com>
2017-12-08 16:23:53 +0800

    init

--- publish-service-web/src/main/java/cn/tianwenjie/publish/system/service/UserService.java
+++ publish-service-web/src/main/java/cn/tianwenjie/publish/system/service/UserService.java
@@ -1,10 +1,7 @@
 package cn.tianwenjie.publish.system.service;
 
-import cn.tianwenjie.publish.system.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
  * Created by tianwj on 2017/12/4.
@@ -12,8 +9,8 @@ import javax.annotation.Resource;
 @Service
 @Slf4j
 public class UserService {
-  @Resource
-  private UserMapper userMapper;
+  //  @Resource
+  //  private UserMapper userMapper;
 
   public boolean login(String account, String password) {
     return false;
--- publish-service-web/src/main/java/cn/tianwenjie/publish/system/web/HomeController.java
+++ publish-service-web/src/main/java/cn/tianwenjie/publish/system/web/HomeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -18,8 +19,9 @@ public class HomeController {
   private UserService userService;
 
   @RequestMapping(value = "/login", method = RequestMethod.GET)
+  @ResponseBody
   public String login(String account, String password) {
-//    userService.login(account, password);
+    //    userService.login(account, password);
     String test = "test";
     return test;
   }
